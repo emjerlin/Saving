fd694e9745c4d3b8a92994ac63954a8353487139
emjerlin <emma.jerlin@elev.ga.ntig.se>
2020-09-17 15:25:05 +0200

    Input lagring klar

--- Saving/Program.cs
+++ Saving/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Xml.Serialization;
 
 namespace Saving
 {
@@ -9,12 +10,63 @@ namespace Saving
         {
             Zombie z1 = new Zombie();
 
+            int lyckad1 = 0;
+            
+            int ohp = 0;
+
+            while (lyckad1!=1){
             Console.WriteLine("How much hp do you want the zombie to have?");
 
-            Console.ReadLine();
+            string ihp = Console.ReadLine();
 
-            Console.WriteLine("How much dmg do you want the zombie to make?");
-            Console.WriteLine("Does the zombie have a hat?")
+            bool lyckad = int.TryParse(ihp, out ohp);
+            
+            if (lyckad){
+                lyckad1++;
+            }
+            }
+            z1.x = ohp;
+
+            //------------------------
+
+            int lyckad2 = 0;
+            
+            int odmg = 0;
+
+            while (lyckad2!=1){
+              Console.WriteLine("How much dmg do you want the zombie to make?");
+
+            string idmg = Console.ReadLine();
+
+            bool lyckad = int.TryParse(idmg, out odmg);
+            
+            if (lyckad){
+                lyckad2++;
+            }
+            }
+            z1.x = odmg;
+
+             //------------------------
+
+            string hat;
+            
+            int lyckad3 = 0;
+
+            while (lyckad3!=1){
+            Console.WriteLine("Does the zombie have a hat?");
+            
+            hat = Console.ReadLine();
+           
+            if (hat == "yes"){
+                z1.hat = true;
+                lyckad3++;
+            }
+            if (hat == "no"){
+                z1.hat = false;
+                lyckad3++;
+            }
+             }
+
 
         }
     }
